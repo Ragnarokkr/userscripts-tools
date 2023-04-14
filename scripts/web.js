eac3985f17e88ce8079a9f5e8e55feef8eca9f52
ragnarokkr <marco.trulla@gmail.com>
2023-04-14 18:04:06 +0200

    Add tool to retrieve YouTube video's cover image

--- scripts/web.js
+++ scripts/web.js
@@ -1,15 +1,18 @@
 // ==UserScript==
 // @name         Userscripts Tools - Web
 // @description  Misc tools to process web pages.
-// @version      0.1.1
+// @version      0.2
 // @author       ragnarøkkr
 // @match        *://*/*
 // @grant        GM_openInTab
 // @grant        GM_registerMenuCommand
+// @grant        GM_notification
 // @noframes
 // ==/UserScript==
 
 (() => {
+    const SCRIPT_NAME = 'Userscripts Tools - Web';
+
 
     // Search on Google for a cached version of current active webpage.
     const searchInGoogleCache = () => {
@@ -23,9 +26,28 @@
         searchInGoogleCache();
     }
 
+    // Retrieve current YouTube video's cover image.
+    const retrieveYouTubeCover = () => {
+        if (!document.location.href.match(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i)) {
+            GM_notification({
+                title: `${SCRIPT_NAME} (Retrieve YouTube Cover)`,
+                text: "Current active page must be a valid YouTube video.",
+                ondone: () => { }
+            });
+            return;
+        }
+        const url = document.querySelector('link[itemprop="thumbnailUrl"]').href;
+        GM_openInTab(url);
+    }
+
+    const onClick_RetrieveYouTubeCover = () => {
+        retrieveYouTubeCover();
+    }
+
     // Registering the menu
     const menu = [
-        { title: 'Search for current webpage in Google\'s cache', callback: onClick_SearchInGoogleCache, shortcut: 'l' }
+        { title: 'Search for current webpage in Google\'s cache', callback: onClick_SearchInGoogleCache, shortcut: 'g' },
+        { title: 'Retrieve current YouTube video\'s cover image', callback: onClick_RetrieveYouTubeCover, shortcut: 'y' }
     ];
 
     for (const menuItem of menu) {
