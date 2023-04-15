744795793a67abec367c1f65949e5335ff0947b6
ragnarokkr <marco.trulla@gmail.com>
2023-04-15 10:15:37 +0200

    Change indenting via editorconfig file

--- scripts/web.user.js
+++ scripts/web.user.js
@@ -11,46 +11,46 @@
 // ==/UserScript==
 
 (() => {
-    const SCRIPT_NAME = 'Userscripts Tools - Web';
+  const SCRIPT_NAME = 'Userscripts Tools - Web';
 
 
-    // Search on Google for a cached version of current active webpage.
-    const searchInGoogleCache = () => {
-        let url = `https://webcache.googleusercontent.com/search?q=cache:${document.location.origin}${document.location.pathname}`;
-        GM_openInTab(url, {
-            active: true
-        });
-    };
+  // Search on Google for a cached version of current active webpage.
+  const searchInGoogleCache = () => {
+    let url = `https://webcache.googleusercontent.com/search?q=cache:${document.location.origin}${document.location.pathname}`;
+    GM_openInTab(url, {
+      active: true
+    });
+  };
 
-    const onClick_SearchInGoogleCache = () => {
-        searchInGoogleCache();
+  const onClick_SearchInGoogleCache = () => {
+    searchInGoogleCache();
+  }
+
+  // Retrieve current YouTube video's cover image.
+  const retrieveYouTubeCover = () => {
+    if (!document.location.href.match(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i)) {
+      GM_notification({
+        title: `${SCRIPT_NAME} (Retrieve YouTube Cover)`,
+        text: "Current active page must be a valid YouTube video.",
+        ondone: () => { }
+      });
+      return;
     }
+    const url = document.querySelector('link[itemprop="thumbnailUrl"]').href;
+    GM_openInTab(url);
+  }
 
-    // Retrieve current YouTube video's cover image.
-    const retrieveYouTubeCover = () => {
-        if (!document.location.href.match(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i)) {
-            GM_notification({
-                title: `${SCRIPT_NAME} (Retrieve YouTube Cover)`,
-                text: "Current active page must be a valid YouTube video.",
-                ondone: () => { }
-            });
-            return;
-        }
-        const url = document.querySelector('link[itemprop="thumbnailUrl"]').href;
-        GM_openInTab(url);
-    }
+  const onClick_RetrieveYouTubeCover = () => {
+    retrieveYouTubeCover();
+  }
 
-    const onClick_RetrieveYouTubeCover = () => {
-        retrieveYouTubeCover();
-    }
+  // Registering the menu
+  const menu = [
+    { title: 'Search for current webpage in Google\'s cache', callback: onClick_SearchInGoogleCache, shortcut: 'g' },
+    { title: 'Retrieve current YouTube video\'s cover image', callback: onClick_RetrieveYouTubeCover, shortcut: 'y' }
+  ];
 
-    // Registering the menu
-    const menu = [
-        { title: 'Search for current webpage in Google\'s cache', callback: onClick_SearchInGoogleCache, shortcut: 'g' },
-        { title: 'Retrieve current YouTube video\'s cover image', callback: onClick_RetrieveYouTubeCover, shortcut: 'y' }
-    ];
-
-    for (const menuItem of menu) {
-        GM_registerMenuCommand(menuItem.title, menuItem.callback, menuItem.shortcut);
-    }
+  for (const menuItem of menu) {
+    GM_registerMenuCommand(menuItem.title, menuItem.callback, menuItem.shortcut);
+  }
 })();
